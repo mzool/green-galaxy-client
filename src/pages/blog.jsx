e76a46a4b6da20e11f8589256c9417331ed6269e
mzool <mohammad_alzoul97@yahoo.com>
2024-02-01 12:33:52 +0300

    nothing

--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -22,11 +22,6 @@ function Blog() {
           mode: "cors",
           method: "get",
           credentials: "include",
-          headers: {
-            Authorization: `GreenBearer ${
-              import.meta.env.VITE_authorization_token
-            }`,
-          },
         }
       )
         .then((res) => res.json())
